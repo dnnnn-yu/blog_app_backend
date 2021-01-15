43d969af8b125b278becb11a847ba3b2d2bdcbc6
yuta dammatsu <yuta.dammatsu@techouse.jp>
2021-01-15 15:40:48 +0900

    cors の設定を追加

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,11 @@ module SampleApi
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.action_dispatch.default_headers = {
+          'Access-Control-Allow-Credentials' => 'true',
+          'Access-Control-Allow-Origin' => 'http://localhost:3001', # アクセス元のURL
+          'Access-Control-Request-Method' => '*' # 許可するメソッド（GET,POST,DELETEなど）：'*'は全てのメソッドという意味
+        }
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
